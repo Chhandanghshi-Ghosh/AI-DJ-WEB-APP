08ac2e64873009b28e302cbbf4ff59690304d039
Chhandanghshi-Ghosh <73993378+Chhandanghshi-Ghosh@users.noreply.github.com>
2021-02-09 17:52:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 song=""
+scoreLeftWrist=0
 leftWristX=0
 leftWristY=0
 rightWristY=0
 rightWristX=0
 
 function preload(){
-    song = loadSound("ANPANMAN.mp3");
+    song = loadSound("EGO.mp3");
 }
 
 function setup(){
-    canvas= createCanvas(500,400)
+    canvas= createCanvas(500,500)
     canvas.center()
 
     video= createCapture(VIDEO)
@@ -27,6 +28,10 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results)
+
+        scoreLeftWrist=results[0].pose.keypoints[9].score
+        console.log('keypoints=' + scoreLeftWrist) 
+
         leftWristX=results[0].pose.leftWrist.x
         leftWristY=results[0].pose.leftWrist.y
         console.log("Left WristX=" + leftWristX + "Left WristY=" + leftWristY)
@@ -39,7 +44,20 @@ function gotPoses(results){
 }
 
 function draw(){
-    image(video,0,0,500,400)
+    image(video,0,0,500,500);
+    fill('#fa0000');
+    stroke('#fa0000');
+    
+    if(scoreLeftWrist > 0.2){
+    circle(leftWristX,leftWristY,20);
+    number= Number(leftWristY);
+    number_remove= floor(number);
+    volume= number_remove/500;
+
+    document.getElementById("volumeh4").innerHTML='Volume:' + volume;
+    song.setVolume(volume);
+    console.log('volume=' + volume)
+    }
 }
 
 function playButton(){
