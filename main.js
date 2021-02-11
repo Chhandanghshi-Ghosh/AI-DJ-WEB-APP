a15bc07f6cb1f85c68262733070a115f4f3479ac
Chhandanghshi-Ghosh <73993378+Chhandanghshi-Ghosh@users.noreply.github.com>
2021-02-11 18:51:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 song=""
 scoreLeftWrist=0
+scoreRightWrist=0
 leftWristX=0
 leftWristY=0
 rightWristY=0
@@ -10,7 +11,7 @@ function preload(){
 }
 
 function setup(){
-    canvas= createCanvas(500,500)
+    canvas= createCanvas(600,500)
     canvas.center()
 
     video= createCapture(VIDEO)
@@ -30,7 +31,10 @@ function gotPoses(results){
         console.log(results)
 
         scoreLeftWrist=results[0].pose.keypoints[9].score
-        console.log('keypoints=' + scoreLeftWrist) 
+        console.log('scoreLeftWrist=' + scoreLeftWrist) 
+
+        scoreRightWrist=results[0].pose.keypoints[10].score
+        console.log('scoreRightWrist=' + scoreRightWrist) 
 
         leftWristX=results[0].pose.leftWrist.x
         leftWristY=results[0].pose.leftWrist.y
@@ -44,15 +48,39 @@ function gotPoses(results){
 }
 
 function draw(){
-    image(video,0,0,500,500);
+    image(video,0,0,600,500);
     fill('#fa0000');
     stroke('#fa0000');
+    if(scoreRightWrist > 0.2 ){
+        circle(rightWristX,rightWristY,20)
+        if(rightWristY > 0 && rightWristY <= 100){
+            document.getElementById('speed').innerHTML="Speed= 0.5x"
+            song.rate(0.5)
+        }
+        else if(rightWristY > 100 && rightWristY <= 200){
+            document.getElementById('speed').innerHTML="Speed= 1x"
+            song.rate(1)
+        }
+        else if(rightWristY > 200 && rightWristY <= 300){
+            document.getElementById('speed').innerHTML="Speed= 1.5x"
+            song.rate(1.5)
+        }
+        else if(rightWristY > 300 && rightWristY <= 400){
+            document.getElementById('speed').innerHTML="Speed= 2x"
+            song.rate(2)
+        }
+        else if(rightWristY > 400 && rightWristY <= 500){
+            document.getElementById('speed').innerHTML="Speed= 2.5x"
+            song.rate(2.5)
+        }
+    }
     
+
     if(scoreLeftWrist > 0.2){
     circle(leftWristX,leftWristY,20);
     number= Number(leftWristY);
-    number_remove= floor(number);
-    volume= number_remove/500;
+    number= toFixed(0);
+    volume= number/500;
 
     document.getElementById("volumeh4").innerHTML='Volume:' + volume;
     song.setVolume(volume);
