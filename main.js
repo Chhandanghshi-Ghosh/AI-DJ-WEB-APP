1572fe0cf7321f78d915ac4ace5d4eb5f26c38ec
Chhandanghshi-Ghosh <73993378+Chhandanghshi-Ghosh@users.noreply.github.com>
2021-04-01 15:45:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,99 +1,40 @@
-song=""
-scoreLeftWrist=0
-scoreRightWrist=0
-leftWristX=0
-leftWristY=0
-rightWristY=0
-rightWristX=0
+videoImage=""
+status=""
+sliderV= ""
 
 function preload(){
-    song = loadSound("EGO.mp3");
+    videoImage= createVideo('video.mp4')
+    videoImage.hide()
+
 }
 
 function setup(){
-    canvas= createCanvas(600,500)
-    canvas.center()
+canvas= createCanvas(300,300)
+canvas.position(500,230)
 
-    video= createCapture(VIDEO)
-    video.hide()
-
-    posenet=ml5.poseNet(video,modelLoaded)
-    posenet.on('pose',gotPoses)
-
-}
-
-function modelLoaded(){
-    console.log("Model Loaded!!!!")
-}
-
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results)
-
-        scoreLeftWrist=results[0].pose.keypoints[9].score
-        console.log('scoreLeftWrist=' + scoreLeftWrist) 
-
-        scoreRightWrist=results[0].pose.keypoints[10].score
-        console.log('scoreRightWrist=' + scoreRightWrist) 
-
-        leftWristX=results[0].pose.leftWrist.x
-        leftWristY=results[0].pose.leftWrist.y
-        console.log("Left WristX=" + leftWristX + "Left WristY=" + leftWristY)
-
-        rightWristX=results[0].pose.rightWrist.x
-        rightWristY=results[0].pose.rightWrist.y
-        console.log("Right WristX=" + rightWristX + "Right WristY=" + rightWristY)
-
-    }
 }
 
 function draw(){
-    image(video,0,0,600,500);
-    fill('#fa0000');
-    stroke('#fa0000');
-    if(scoreRightWrist > 0.2 ){
-        circle(rightWristX,rightWristY,20)
-        if(rightWristY > 0 && rightWristY <= 100){
-            document.getElementById('speed').innerHTML="Speed= 0.5x"
-            song.rate(0.5)
-        }
-        else if(rightWristY > 100 && rightWristY <= 200){
-            document.getElementById('speed').innerHTML="Speed= 1x"
-            song.rate(1)
-        }
-        else if(rightWristY > 200 && rightWristY <= 300){
-            document.getElementById('speed').innerHTML="Speed= 1.5x"
-            song.rate(1.5)
-        }
-        else if(rightWristY > 300 && rightWristY <= 400){
-            document.getElementById('speed').innerHTML="Speed= 2x"
-            song.rate(2)
-        }
-        else if(rightWristY > 400 && rightWristY <= 500){
-            document.getElementById('speed').innerHTML="Speed= 2.5x"
-            song.rate(2.5)
-        }
-    }
-    
-
-    if(scoreLeftWrist > 0.2){
-    circle(leftWristX,leftWristY,20);
-    number= Number(leftWristY);
-    number= toFixed(0);
-    volume= number/500;
-
-    document.getElementById("volumeh4").innerHTML='Volume:' + volume;
-    song.setVolume(volume);
-    console.log('volume=' + volume)
-    }
+    image(videoImage, 0 , 0 , 300, 300)
 }
 
-function playButton(){
-    song.play()
-    song.setVolume(0.4)
-    song.rate(1)
- }
+function start(){
+objectDetector= ml5.objectDetector('cocossd',modelLoaded)
+document.getElementById("status").innerHTML="Status: Detecting Objects."
+}
 
- function stopButton(){
-     song.stop()
- }
+function sliDer(){
+    sliderV=document.getElementById("slider").value
+    videoImage.volume(sliderV)
+}
+
+function modelLoaded(){
+console.log("Model Loaded!!")
+status= "true"
+videoImage.loop()
+videoImage.speed(1)
+videoImage.volume(0)
+}
+
+
+
